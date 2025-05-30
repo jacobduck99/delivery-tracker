c0cbdf9c24fa288c5bd402c9729c2b95e3a47045
jacobduck99 <jacob.formaz@gmail.com>
2025-05-30 12:10:53 +1000

    Fix schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,4 @@
-CREATE TABLE IF NOT EXISTS run ;
-
-CREATE TABLE run (
+CREATE TABLE IF NOT EXISTS run (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     van_number INTEGER NOT NULL,
     van_name TEXT NOT NULL,
@@ -9,13 +7,13 @@ CREATE TABLE run (
     number_of_drops INTEGER NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS deliveries (
-  id        INTEGER PRIMARY KEY AUTOINCREMENT,
-  run_id    INTEGER,            
-  drop_idx  INTEGER,            
-  start_ts  TEXT,
-  end_ts    TEXT,
-  elapsed   INTEGER,           
-  FOREIGN KEY(run_id) REFERENCES run(id)
-);
 
+CREATE TABLE IF NOT EXISTS deliveries (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    run_id    INTEGER,
+    drop_idx  INTEGER,
+    start_ts  TEXT,
+    end_ts    TEXT,
+    elapsed   INTEGER,
+    FOREIGN KEY (run_id) REFERENCES run(id)
+);
