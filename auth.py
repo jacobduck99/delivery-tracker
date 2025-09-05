3005e37fb0f7fb5c671e7b174f2bac9ecfabacf5
Jacob Duckworth <jacob.formaz@gmail.com>
2025-09-05 13:13:08 +1000

    Fixed typo lol

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 from flask_login import UserMixin
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
-from databsae import get_db, init_db, close_db
+from database import get_db, init_db, close_db
 
 class User(UserMixin):
     def __init__(self, id, email, password_hash):
