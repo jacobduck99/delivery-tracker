5aeca46c8d01da2ccaf6f1b3b0da4f4addfe4f10
Jacob Duckworth <jacob.formaz@gmail.com>
2025-09-11 12:17:31 +1000

    Added js listener for buttons and added them into local storage

--- static/js/storage.js
+++ static/js/storage.js
@@ -1,10 +1,14 @@
-const arrived = document.getElementsByClassName('arrived-btn');
+
+document.querySelectorAll(".arrived-btn, .delivered-btn").forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    const form = e.target.closest("form");
+    const dropIndex = form.querySelector("input[name='drop_index']").value;
+    const action = e.target.value; // will be "start" or "stop"
+    localStorage.setItem(`drop-${dropIndex}`, action);
+  });
+});
 
 
-arrived.AddEventListener("click", );
 
 
-function AddButtonsLocalStorage() {
-    const btn = document.querySelector('button');
-    const 
-}
+
