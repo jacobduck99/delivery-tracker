bb44a22c0dfe1fb58c49ebfa0b46bb5e401c2332
Jacob Duckworth <jacob.formaz@gmail.com>
2025-09-11 11:40:58 +1000

    Added class in html so I can grab it in js for my arrived and delivered to go into local storage

---
+++ static/js/storage.js
@@ -0,0 +1,10 @@
+const arrived = document.getElementsByClassName('arrived-btn');
+
+
+arrived.AddEventListener("click", );
+
+
+function AddButtonsLocalStorage() {
+    const btn = document.querySelector('button');
+    const 
+}
