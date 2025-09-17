1881b6fc78ff04e24a83e27e25890261f0505755
Jacob Duckworth <jacob.formaz@gmail.com>
2025-09-17 12:35:42 +1000

    Updated schema.sql to have these columns now

--- schema.sql
+++ schema.sql
@@ -17,6 +17,8 @@ create table if not exists run (
   first_break     text    not null,
   second_break    text    not null,
   end_time        text,
+  actual_end_time_at text,
+  duration_ms     integer,
   number_of_drops integer not null,
   truck_damage text DEFAULT NULL CHECK (truck_damage IS NULL OR length(truck_damage) <= 255), 
   foreign key(user_id) references users(id) on delete cascade
