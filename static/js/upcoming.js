8174c001f3be26dec8ca5ead96809af0da69066f
Jacob Duckworth <jacob.formaz@gmail.com>
2025-10-22 13:18:04 +1100

    Writing logic to make cards nicer with upcoming drops and current drops

--- static/js/upcoming.js
+++ static/js/upcoming.js
@@ -1,17 +1,35 @@
 
-function initialiseQueueFromDom() {
-    const currentDrop = document.getElementById("current-drop-slot");
-    const upcomingDrops = document.getElementById("upcoming-list");
-    const allDrops = [ ...document.querySelectorAll('.drop-card[id^="drop-"]')];
+// function initialiseQueueFromDom() {
+//     const currentDrop = document.getElementById("current-drop-slot");
+//     const upcomingDrops = document.getElementById("upcoming-list");
+//     const allDrops = [ ...document.querySelectorAll('.drop-card[id^="drop-"]')];
 
-    for (let i = 0; i < allDrops.length; i++) {
-        if (i === 0) {
-            currentDrop.append(allDrops[i]);
-        } else {
-            upcomingDrops.append(allDrops[i]);
-        }
-    }
+//     for (let i = 0; i < allDrops.length; i++) {
+//         if (i === 0) {
+//             currentDrop.append(allDrops[i]);
+//         } else {
+//             upcomingDrops.append(allDrops[i]);
+//         }
+//     }
 
-};
+// };
 
-initialiseQueueFromDom();
+// function promoteNextDrop() {
+//   const currentDrop = document.getElementById("current-drop-slot");
+//   const upcomingDrops = document.getElementById("upcoming-list");
+
+//   // only promote if current slot is empty
+//   if (currentDrop.children.length === 0) {
+//     const nextDrop = upcomingDrops.querySelector(".drop-card"); // first drop in upcoming
+
+//     if (nextDrop) {
+//       currentDrop.append(nextDrop);
+//       console.log("Promoted next drop to current slot!");
+//     } else {
+//       console.log("No upcoming drops left to promote.");
+//     }
+//   }
+// }
+
+// initialiseQueueFromDom();
+
